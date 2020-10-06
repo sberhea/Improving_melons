ee3716370f8f86139a892a9b981d0aad2fa76f1e
vagrant <vagrant@vagrant.vm>
2020-10-06 04:12:43 +0000

    Rough draft

--- melons.py
+++ melons.py
@@ -1,3 +1,4 @@
+"""
 melon_names = {
     1: 'Honeydew',
     2: 'Crenshaw',
@@ -20,4 +21,67 @@ melon_seedlessness = {
     3: False,
     4: False,
     5: False,
+
+flesh_color = {
+    1: Green,
+    2: Orange,
+    3: Red,
+    4: Blue,
+    5: Purple,
+}
+
+weight = {
+    1: 5.00,
+    2: 3.00,
+    3: 2.50,
+    4: 2.50,
+    5: 4.00,
+}
+
+rind_color = {
+    1: Green,
+    2: Green,
+    3: Green,
+    4: Green,
+    5: Green,
+}
+
+} """
+
+melon_names = {
+    'Honeydew' : {
+        'melon_prices' : 0.99,
+        'melon_seedlessness' : True,
+        'flesh_color' : None,
+        'weight' : None,
+        'rind_color' : None,
+        },
+    'Crenshaw' : {
+        'melon_prices': 2.00, 
+        'melon_seedlessness': False,
+        'flesh_color' : None,
+        'weight' : None,
+        'rind_color' : None,
+        },
+    'Crane' : {
+        'melon_prices': 2.50, 
+        'melon_seedlessness': False,
+        'flesh_color' : None,
+        'weight' : None,
+        'rind_color' : None,
+        },
+    'Casaba' : {
+        'melon_prices': 2.50, 
+        'melon_seedlessness': False,
+        'flesh_color' : None,
+        'weight' : None,
+        'rind_color' : None,
+        },
+    'Cantoloupe' : {
+        'melon_prices': 0.99, 
+        'melon_seedlessness': False,
+        'flesh_color' : None,
+        'weight' : None,
+        'rind_color' : None,
+        },
 }
